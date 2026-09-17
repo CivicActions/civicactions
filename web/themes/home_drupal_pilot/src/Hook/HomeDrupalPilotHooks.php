46b2a5158f20d7db7d3c46f46fdc94d41d32d7f8
Jack Haas <jack.haas@civicactions.com>
2026-09-17 15:36:16 -0400

    fix(security): sanitize SDC URLs and align CTA attributes

--- web/themes/home_drupal_pilot/src/Hook/HomeDrupalPilotHooks.php
+++ web/themes/home_drupal_pilot/src/Hook/HomeDrupalPilotHooks.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\home_drupal_pilot\Hook;
 
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Security\Attribute\TrustedCallback;
 
 /**
  * Hook implementations for Home Drupal Pilot.
@@ -26,6 +28,34 @@ class HomeDrupalPilotHooks {
     if (isset($data['preview']['#access']) && $data['preview']['#access'] === FALSE) {
       unset($data['preview']);
     }
+  }
+
+  /**
+   * Adds URL protocol filtering to SDC component props.
+   */
+  #[Hook('element_info_alter')]
+  public function elementInfoAlter(array &$info): void {
+    $info['component']['#propsAlter'][] = [self::class, 'sanitizeComponentUrls'];
+  }
+
+  /**
+   * Removes dangerous protocols from URL props before component rendering.
+   *
+   * @param array<string, mixed> $props
+   *   Component properties.
+   *
+   * @return array<string, mixed>
+   *   Sanitized component properties.
+   */
+  #[TrustedCallback]
+  public static function sanitizeComponentUrls(array $props): array {
+    foreach (['src', 'primary_button_url', 'secondary_button_url'] as $property) {
+      if (isset($props[$property]) && is_string($props[$property])) {
+        $props[$property] = UrlHelper::stripDangerousProtocols($props[$property]);
+      }
+    }
+
+    return $props;
   }
 
 }
